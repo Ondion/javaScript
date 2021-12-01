80f257342f071ccaf2c38c065e6c730c88367dcc
Fabio N. Xavier <fabio.xavier@live.com>
2021-11-30 23:23:40 -0300

    atualização dos exes

--- Exercícios/exe.js
+++ Exercícios/exe.js
@@ -1,17 +1,20 @@
-// A Trybe abriu uma lanchonete e precisa criar um menu de opções para que as pessoas consigam escolher qual lanche maravilhoso irão pedir.
-// Para isso escreva um algoritmo que recebe o número da opção escolhida conforme tabela abaixo e retorne a string inteira do respectivo lanche:
-// "1 - Trybe Lanche Feliz"
-// "2 - McTrybe"
-// "3 - TrybeWooper"
-// "4 - X-Trybe"
-// "5 - Triplo Trybe com JS"
-// Caso a pessoa escolha uma opção que não existe na tabela acima, retorne:
-// -------------------------------
-// "Não temos esta opção ainda :("
-// -------------------------------
-// Utilize Switch/Case para resolver este problema.
+/* A Trybe abriu uma lanchonete e precisa criar um menu de opções para que as pessoas consigam escolher qual lanche maravilhoso irão pedir.
+Para isso escreva um algoritmo que recebe o número da opção escolhida conforme tabela abaixo e retorne a string inteira do respectivo lanche:
 
-let numero = 1;
+"1 - Trybe Lanche Feliz"
+"2 - McTrybe"
+"3 - TrybeWooper"
+"4 - X-Trybe"
+"5 - Triplo Trybe com JS"
+
+Caso a pessoa escolha uma opção que não existe na tabela acima, retorne:
+-------------------------------
+"Não temos esta opção ainda :("
+-------------------------------
+
+Utilize Switch/Case para resolver este problema.*/
+
+let numero = 8;
 
 
 switch(numero){
@@ -29,6 +32,7 @@ switch(numero){
         break;
     case 5:
         console.log("Triplo Trybe com JS")
+        break
     default:
         console.log("Não temos esta opção ainda :(")
 }
