4c2d9986356544889acebd837d8b6bf0f38d8795
Fabio N. Xavier <fabio.xavier@live.com>
2022-01-23 18:14:00 -0300

    adicionando conteúdo e spread

---
+++ Exercícios/Spread Operator/exemplo.js
@@ -0,0 +1,15 @@
+//Exemplo de uso do Spread Operator.
+
+const people = {
+  id: 101,
+  name: 'Alysson',
+  age: 25,
+};
+
+const about = {
+  address: 'Av. Getúlio Vargas, 1000',
+  occupation: 'Developer',
+};
+
+const customer = { ...people, ...about };
+console.log(customer);
