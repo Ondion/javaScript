cd23aedce5eb9b6ffe6ed2d81167a2000c04dc23
Fabio N. Xavier <fabio.xavier@live.com>
2022-01-24 15:12:42 -0300

    update

---
+++ Exercícios/Object Destructuring/filtroComO.D.js
@@ -0,0 +1,36 @@
+// Função que filtra resultados, fazendo uso de O.D.
+
+const people = [
+  {
+    name: 'Nicole',
+    bornIn: 1992,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Harry',
+    bornIn: 2008,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Toby',
+    bornIn: 1901,
+    nationality: 'Australian',
+  },
+  {
+    name: 'Frida',
+    bornIn: 1960,
+    nationality: 'Dannish',
+  },
+  {
+    name: 'Fernando',
+    bornIn: 2001,
+    nationality: 'Brazilian',
+  },
+];
+
+const filtro = (callback) => 0;
+
+filtro(people.forEach(element => {
+  { name, bornIn, nationality }
+}))
+console.log(people)
