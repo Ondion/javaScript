49bd202391575cbe8c30fa1d0bd6036d2630a6bd
Fabio N. Xavier <fabio.xavier@live.com>
2022-01-24 22:32:33 -0300

    update

--- Exercícios/Object Destructuring/filtroComO.D.js
+++ Exercícios/Object Destructuring/filtroComO.D.js
@@ -1,36 +1,129 @@
 // Função que filtra resultados, fazendo uso de O.D.
 
-const people = [
-  {
-    name: 'Nicole',
-    bornIn: 1992,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Harry',
-    bornIn: 2008,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Toby',
-    bornIn: 1901,
-    nationality: 'Australian',
-  },
-  {
-    name: 'Frida',
-    bornIn: 1960,
-    nationality: 'Dannish',
-  },
-  {
-    name: 'Fernando',
-    bornIn: 2001,
-    nationality: 'Brazilian',
-  },
-];
+// const people = [
+//   {
+//     name: 'Nicole',
+//     bornIn: 1992,
+//     nationality: 'Australian',
+//   },
+//   {
+//     name: 'Harry',
+//     bornIn: 2008,
+//     nationality: 'Australian',
+//   },
+//   {
+//     name: 'Toby',
+//     bornIn: 1901,
+//     nationality: 'Australian',
+//   },
+//   {
+//     name: 'Frida',
+//     bornIn: 1960,
+//     nationality: 'Dannish',
+//   },
+//   {
+//     name: 'Fernando',
+//     bornIn: 2001,
+//     nationality: 'Brazilian',
+//   },
+// ];
 
-const filtro = (callback) => 0;
+// const filtro = (element) => console.log(element);
 
-filtro(people.forEach(element => {
-  { name, bornIn, nationality }
-}))
-console.log(people)
+// let teste = people.forEach(element => {
+//   filtro(element.nationality === "Australian" ? element : let a = 0) 
+// });
+
+// const numbers = [3, 12, 1, 21, 23];
+// const foundElements = numbers.find(e => e > 10);
+// console.log(foundElements);
+
+
+          
+// const products = [
+//   {
+//     name: 'laptop',
+//     price: 1000,
+//     id: 3,
+//   },
+//   {
+//     name: 'desktop',
+//     price: 1500,
+//     id: 2
+//   },
+//   {
+//     name: 'phone',
+//     price: 500,
+//     id: 1
+//   }
+// ];
+
+// console.log(products.sort((a,b) => b.price - a.price));
+
+        
+
+
+          
+const adultos = [
+  {
+    peso: 100,
+    altura: 1.85
+  },
+  {
+    peso: 90,
+    altura: 1.72
+  },
+  {
+    peso: 79,
+    altura: 1.99
+  }
+]
+
+// IMC = Índice de Massa Corporal
+
+// function IMC(adulto) {
+//   return parseFloat((adulto.peso/(Math.pow(adulto.altura,2))).toFixed(2))
+// }
+
+// console.log(adultos.map(IMC))
+
+
+          
+// const cart = [
+//   {
+//     name: "laptop",
+//     price: 1000,
+//     quantity: 2,
+//     id: 3,
+//   },
+//   {
+//     name: "desktop",
+//     price: 1500,
+//     quantity: 1,
+//     id: 2,
+//   },
+//   {
+//     name: "phone",
+//     price: 500,
+//     quantity: 3,
+//     id: 1,
+//   },
+// ];
+            
+// const valorInicial = 0;
+// const totalCart = cart.reduce((accumulator, product) => accumulator + product.price * product.quantity, valorInicial);
+
+// console.log(totalCart);
+
+// const firstArray = [3, 5, 7];
+            
+// const secondArray = [2, ...firstArray, 4]
+
+// console.log(secondArray)
+
+          
+
+          
+const newBook = (title, author, price, discount) => (EXPRESSÃO);
+
+        
