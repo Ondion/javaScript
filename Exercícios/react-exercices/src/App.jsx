b0258a9f0dcd2b60b8f41687369eb8d624ec4512
Fabio N. Xavier <fabio.xavier@live.com>
2022-02-19 16:21:12 -0300

    aplicando melhorias

--- Exercícios/react-exercices/src/App.jsx
+++ Exercícios/react-exercices/src/App.jsx
@@ -1,55 +1,14 @@
 import React from "react";
-// import Card from "./components/cards"
 import "../src/App.css"
-// import Main from "./Main";
-// import LadoA from "./LadoA"
-// import LadoB from "./LadoB"
 
 
-const objeto = {
-  valor1: 'objeto',
-  valor2: 'exemplo',
-  valor3: 'react',
-}
+const aqui = (x) => console.log(x);
 
-const arrayObjeto = [objeto, objeto, objeto];
-
+const fabio = "fabio"
 function App() {
   return (
-      <p> {arrayObjeto.map((element) => {
-      return <ListaRender {...element}/> 
-  })} 
-      </p>
+   <button onClick={() => aqui(fabio)}>React!</button>
   )
 }
-
-
-
-function ListaRender ({ valor1, valor2, valor3 }) {
-
-  return (
-    <div>
-      <p>{valor1}</p>
-      <p>{valor2}</p>
-      <p>{valor3}</p>
-    </div>
-  )
-}
-
 
 export default App;
-
-
-// const obj = {
-//   name: "Fábio",
-//   lastName: "Xavier",
-// };
-
-// const array = ['fabio', 'nogueira', 'xavier']
-
-// const App = () => array.map((e) => <h1>{e}</h1>)
-  
-  
-//   {/* // {Object.values(obj).map((e) => <h1>{e}</h1>)} */}
-//   {/* // <h1>.  |  .</h1> */}
-//   {/* // <h1>{array}</h1> */}
