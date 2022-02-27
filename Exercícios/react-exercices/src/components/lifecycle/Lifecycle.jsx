df044d1c32e7ebc435ba489d0831fcb6a73fd660
Fabio N. Xavier <fabio.xavier@live.com>
2022-02-27 20:37:48 -0300

    atualizando conteúdo didático sobre react

---
+++ Exercícios/react-exercices/src/components/lifecycle/Lifecycle.jsx
@@ -0,0 +1,36 @@
+import React from "react";
+
+class Lifecycle extends React.Component {
+  constructor() {
+    super();
+    console.log('constructor');
+
+    this.state = {
+      piada: 'myJoke',
+    }
+  }
+
+  componentDidMount() {
+    console.log('componentDidMount');
+    alert('oi')
+    this.setState({
+      piada: 'mudou!'
+    })
+  }
+
+  componentDidUpdate() {
+    console.log('componentDidUpdate');
+  }
+
+  render() {
+    console.log('render');
+    return(
+      <>
+      <p>{this.state.piada}</p>
+      <button onClick={this.componentDidUpdate}>Aqui</button>
+      </>
+    )
+  }
+}
+
+export default Lifecycle;
--- Exercícios/react-exercices/src/index.js
+++ Exercícios/react-exercices/src/index.js
@@ -5,5 +5,8 @@ import ReactDOM from 'react-dom';
 // import AppFreeCodeCamp from './components/freeCodeCamp/AppFreeCodeCamp.jsx'
 // ReactDOM.render(<AppFreeCodeCamp />, document.getElementById('root'));
  
-import AppFormularios from './components/formulários/appFormularios';
-ReactDOM.render(<AppFormularios />, document.getElementById('root'))
+// import AppFormularios from './components/formulários/appFormularios';
+// ReactDOM.render(<AppFormularios />, document.getElementById('root'))
+
+import Lifecycle from './components/lifecycle/Lifecycle'
+ReactDOM.render(<Lifecycle />, document.getElementById('root'))
