cf6bdd7ac53fd5c91ed8c6ccc2614197040a367d
Fabio N. Xavier <fabio.xavier@live.com>
2022-03-05 23:51:19 -0300

    conteúdo sobre router/react

--- Exercícios/react-exercices/src/App.jsx
+++ Exercícios/react-exercices/src/App.jsx
@@ -1,13 +1,14 @@
 import React, { Component } from 'react';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css'
 import Sidebar from './Sidebar';
 
 class App extends Component {
   render() {
     return (
-      <>
+      <BrowserRouter>
         <Sidebar />
-      </>
+      </BrowserRouter>
     );
   }
 }
--- Exercícios/react-exercices/src/Sidebar.jsx
+++ Exercícios/react-exercices/src/Sidebar.jsx
@@ -1,10 +1,20 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Sidebar extends Component {
   render() {
     return (
      <aside className='SidebarClass'>
-
+       <Link to='teste' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
+       <Link to='' >Teste</Link>
      </aside>
     );
   }
